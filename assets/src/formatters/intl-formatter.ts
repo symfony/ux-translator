90cbebc289cffbe05ae45dbf425df382449a5514
Hugo Alliaume <hugo@alliau.me>
2025-02-10 23:26:46 +0100

    [Translator] Fix types definition, allow `Date` as parameter type

--- assets/src/formatters/intl-formatter.ts
+++ assets/src/formatters/intl-formatter.ts
@@ -7,7 +7,7 @@ import { IntlMessageFormat } from 'intl-messageformat';
  * @param parameters An array of parameters for the message
  * @param locale     The locale
  */
-export function formatIntl(id: string, parameters: Record<string, string | number>, locale: string): string {
+export function formatIntl(id: string, parameters: Record<string, string | number | Date>, locale: string): string {
     if (id === '') {
         return '';
     }
@@ -23,5 +23,5 @@ export function formatIntl(id: string, parameters: Record<string, string | numbe
         }
     });
 
-    return intlMessage.format(parameters);
+    return intlMessage.format(parameters) as string;
 }
